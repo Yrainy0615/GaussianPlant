8f0bdc76ee1c4fa366eb9c1e1d672b098e29aacc
Yrainy0615 <yrainy0615@outlook.com>
2024-12-10 15:35:42 +0900

    update dockerfile

--- docker/docker_build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-username=yyang
-projectname=gaussianplant
-docker build -t repo-luna.ist.osaka-u.ac.jp:5000/${username}/${projectname}:build .
--- docker_run.sh
+++ docker_run.sh
@@ -1,9 +1,11 @@
 username=yyang
-docker run --gpus all -itd --rm \
+project_name=gs_base
+container_name=SuGaR
+
+docker run --gpus all -itd \
     -u $(id -u $username):$(id -g $username) \
-    --name ${username}_${project_name} \
-    -v /mnt/workspace2024/${username}/GaussianPlant:/home/${username}/mnt/workspace \
+    --name ${username}_${container_name} \
+    -v /mnt/workspace2024/${username}/${container_name}:/home/${username}/mnt/workspace \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    repo-luna.ist.osaka-u.ac.jp:5000/${username}/gaussianplant:build \
-    bash
+    repo-luna.ist.osaka-u.ac.jp:5000/${username}/${project_name}:build \
